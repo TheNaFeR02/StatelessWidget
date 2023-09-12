c62999dfc9ba4e7c52b58e31c0bcfd700427497a
TheNaFeR02 <acunafer.02@gmail.com>
2023-09-12 18:21:53 -0500

    first commit

---
+++ lib/ui/pages/color_selection_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+
+import '../../utils/color_utils.dart';
+import '../widgets/color_palette.dart';
+
+class ColorSelectionPage extends StatelessWidget {
+  const ColorSelectionPage({Key? key}) : super(key: key);
+
+  void showDialog() {
+    Get.dialog(const AlertDialog(
+      title: Text('Flutter'),
+      content: Text('Dialog'),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Color palette app'),
+          actions: [
+            IconButton(onPressed: showDialog, icon: Icon(Icons.info_outline))
+          ],
+        ),
+        // https://colorhunt.co/
+        body: (Center(
+          // child: Text(
+          //     "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
+          child: SingleChildScrollView(
+            child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [ColorPalette(baseColor: '#FFBA86', callback: showColor,),ColorPalette(baseColor: '#D5FFD0', callback: showColor, )],
+            ),
+          ),
+        )));
+  }
+
+  // está es la función que será llamada con cada click a un ColorPalette
+  void showColor(String value) {
+    Clipboard.setData(ClipboardData(text: value));
+    Get.snackbar(
+      'Color palette',
+      'Valor copiado',
+      backgroundColor: ColorUtils.FromHex(value),
+      snackPosition: SnackPosition.BOTTOM,
+    );
+  }
+}
---
+++ lib/ui/widgets/color_palette.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../../utils/color_utils.dart';
+
+class ColorPalette extends StatelessWidget {
+  const ColorPalette({
+    Key? key,
+    // así se reciben en el constructor, por ejemplo
+    required this.baseColor,
+    required this.callback,
+  }) : super(key: key);
+
+  // atributos de la clase, por ejemplo
+  final String baseColor;
+
+  // también se puede tener como un atributo un callback, necesiario ya que es un
+  // widget sin estado, por ejemplo
+  final Function(String) callback;
+
+  // el callback se puede llamar así en el GestureDetector
+
+  @override
+  Widget build(BuildContext context) {
+    // padding para crear espacio alrededor del widget
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        // vamos a mostrar los elementos en un row
+        //   child: Text(
+        //       'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+        // );
+
+        child: Row(
+          children: [
+            Expanded(
+              flex: 4,
+              child: GestureDetector(
+                onTap: () => callback(baseColor.toString()),
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex(baseColor),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: GestureDetector(
+                onTap: () => callback(baseColor.toString()),
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex("000000"),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
